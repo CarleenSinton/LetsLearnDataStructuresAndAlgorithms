1c48a8561c70b9b7745eacc74405703745368f10
CarleenSinton <Carleenshah@gmail.com>
2021-11-05 21:20:03 -0700

    Add a simple defintion for dictionary

--- LetsLearnDataStructuresAndAlgorithms!/Dictionaries.cs
+++ LetsLearnDataStructuresAndAlgorithms!/Dictionaries.cs
@@ -8,7 +8,26 @@ namespace LetsLearnDataStructuresAndAlgorithms_
     {
         public static void DescribeDictionary()
         {
-            Console.WriteLine("This is where I write about Dictionaries");
+            BasicDictionaryDescription();
+        }
+
+        private static void BasicDictionaryDescription()
+        {
+            Console.WriteLine(@"
+             ____  _      _   _                        _           
+            |  _ \(_)    | | (_)                      (_)          
+            | | | |_  ___| |_ _  ___  _ __   __ _ _ __ _  ___  ___ 
+            | | | | |/ __| __| |/ _ \| '_ \ / _` | '__| |/ _ \/ __|
+            | |/ /| | (__| |_| | (_) | | | | (_| | |  | |  __/\__ \
+            |___/ |_|\___|\__|_|\___/|_| |_|\__,_|_|  |_|\___||___/
+                                                       
+            ");
+
+            Console.WriteLine(@"
+            A dictionary in computer science is like a dictionary for words. 
+            The dictonary's keys are like the words.
+            The values are like the definitions.
+            ");
         }
     }
 }
